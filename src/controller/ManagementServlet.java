87181b05715342214f9e4ac4cd4f8d0b5c624ecd
ymaguchi.shun <yamaguchi.shun@m.alhinc.jp>
2017-09-14 14:29:18 +0900

    8回目

--- src/controller/ManagementServlet.java
+++ src/controller/ManagementServlet.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import beans.Branch;
 import beans.Division;
@@ -26,25 +24,16 @@ public class ManagementServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
 
-		HttpSession session = request.getSession();
 		User sessionUser = (User) request.getSession().getAttribute("user") ;
 		List<User> users = new UserService().getUser();
-		List<String> messages = new ArrayList<String>();
 		List<Branch> branches = new BranchService().getBranch();
 		List<Division> divisions = new DivisionService().getDivision();
 
-//		if(sessionUser.getDivisionId()==1){
-			request.setAttribute("sessionUser", sessionUser);
-			request.setAttribute("users", users);
-			request.setAttribute("branches", branches);
-			request.setAttribute("divisions", divisions);
-			request.getRequestDispatcher("/management.jsp").forward(request, response);
-			return;
-/*		}else{
-			messages.add("アクセスエラー");
-			session.setAttribute("errorMessages", messages);
-			response.sendRedirect("./");
-			return;
-		}*/
+		request.setAttribute("sessionUser", sessionUser);
+		request.setAttribute("users", users);
+		request.setAttribute("branches", branches);
+		request.setAttribute("divisions", divisions);
+		request.getRequestDispatcher("/management.jsp").forward(request, response);
+		return;
 	}
 }
