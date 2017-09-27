d229bab3871fb57a5cc5fd6b17a801245fa89550
ymaguchi.shun <yamaguchi.shun@m.alhinc.jp>
2017-09-27 11:37:31 +0900

    12回目

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -11,8 +11,8 @@ public class DBUtil {
 
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/Chapter6";
-	private static final String USER = "root";
-	private static final String PASSWORD = "Incubus1023";
+	private static final String USER = "";
+	private static final String PASSWORD = "";
 
 	static {
 
